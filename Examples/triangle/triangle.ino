e0c1167dd7678c64db8f8f9495273871cd585e43
PrepaBas <151618231+PrepaBas@users.noreply.github.com>
2025-01-31 23:08:25 +0100

    Update triangle.ino

--- Examples/triangle/triangle.ino
+++ Examples/triangle/triangle.ino
@@ -1,34 +1,21 @@
-#include <RobotCoupe.h>
+#include "RobotCoupe.h"
 #include "XNucleoIHM02A1.h"
 
-RobotCoupe robot;
+RobotCoupe robot(263, 34);
 
 void setup(){
     robot.begin();
+    
 }
 
 void loop()
 {   
-    robot.move_straight(1, 1000);
-    robot.wait_stop();
+    for(int i=0; i<3; i++){
+        robot.move_straight(1, 700);
+        robot.wait_stop();
 
-
-    robot.rotate(1, 60);
-    robot.wait_stop();
-
-    robot.move_straight(1, 1000);
-    robot.wait_stop();
-
-
-    robot.rotate(1, 60);
-    robot.wait_stop();
-
-
-    robot.move_straight(1, 1000);
-    robot.wait_stop();
-
-
-    robot.rotate(1, 60);
-    robot.wait_stop();
-    delay(1000);
+        robot.rotate(1, 120);
+        robot.wait_stop();
+    }
+    delay(2000);
 }
